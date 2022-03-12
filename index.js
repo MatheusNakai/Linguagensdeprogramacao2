de9f5d8d00a3cd1cc7da4ce837ecc05a8f12e6a2
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-03-12 12:49:20 -0300

    HTTPS request GET

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 require('dotenv').config()
-console.log(process.env.API_key)
+const axios = require('axios')
 //operador de desestruturação
 
 const { PROTOCOL, BASE_URL, API_key, LANGU, UNITS, Q } = process.env
 const url = `${PROTOCOL}://${BASE_URL}?appid=${API_key}&units=${UNITS}&q=${Q}`
 
-console.log(url)
+axios.get(url)
+.then(res =>{
+    console.log(res.data)
+    return res.data
+})
+.then (res => {
+    console.log(res.cnt)
+    return res
+})
+.then(res =>{
+    return res.list
+})
+.then(res=>{
+    
+})
