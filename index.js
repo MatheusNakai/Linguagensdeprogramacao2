fde14e8b197371653043c1b73b7657d3f9f08519
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-03-12 13:07:51 -0300

    solving the exercise
    weather feels like greater than 30°C

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ axios.get(url)
 .then(res =>{
     return res.list
 })
+// .then(res=>{
+//     for (let previsao of res){
+//         console.log(`
+//         ${new Date (+previsao.dt*1000).toLocaleString()},
+//         Min:${previsao.main.tem_min},
+//         Max:${previsao.main.tem_max},
+//         Umidade:${previsao.main.humidity}%,
+//         ${previsao.weather[0].description}
+//         `)
+//     }
+// })
 .then(res=>{
-    
+    cont = 0
+    for (let previsao of res){
+        if(previsao.main.feels_like>30){
+            cont++
+        }
+    }
+    console.log(`n>30 \n ${cont}`)
 })
