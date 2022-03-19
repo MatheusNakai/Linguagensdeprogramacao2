0f776b1f490fea80cfadf418d3dc5537bcaec189
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-03-19 10:18:10 -0300

    Update async-await.js

--- async-await.js
+++ async-await.js
@@ -4,5 +4,25 @@ function hello (nome){
     })
 }
 
-hello('Pedro')
-.then((res)=>console.log(res))
+
+async function hello (nome){
+    return `Hello ${nome}`
+}
+
+
+function fatorial(n){
+    if(n<0) return Promise.reject("valor nao pode ser negativo")
+    let res = 1
+    for(let i=2;i<=n;i++)res *=i
+    return Promise.resolve(res)
+}
+
+function chamadaComThenCatch(){
+    fatorial(10)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+    fatorial(-10)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
