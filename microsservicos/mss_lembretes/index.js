c50daf764c08f6cad8e920c41104ff23d6dcd6c5
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-03-26 11:12:47 -0300

    Starting the mss_lembretes
    add routes to get and post responses

---
+++ microsservicos/mss_lembretes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/lembretes', (req, res)=>{
+
+})
+
+app.post('/lembretes',(req,res)=>{
+
+})
+
+app.listen(4000,()=>{
+    console.log("Lebretes. Porta 4000")
+})
