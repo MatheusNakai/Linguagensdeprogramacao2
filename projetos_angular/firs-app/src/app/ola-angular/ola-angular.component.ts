7af6b759b6b9a81341cbe179f6eea4cc173e7466
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-30 10:54:06 -0300

    creating a css file to component

--- projetos_angular/firs-app/src/app/ola-angular/ola-angular.component.ts
+++ projetos_angular/firs-app/src/app/ola-angular/ola-angular.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: "ola-angular",
     //template: ``
-    templateUrl: "./ola-angular.component.html"
+    templateUrl: "./ola-angular.component.html",
+    styleUrls: ["./ola-angular.component.css"]
 })
 export class OlaAangularComponent {
     
